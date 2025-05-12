b29a2e257276610257e11e81b91059dc10189937
Junipeeer <181602566+Junipeeer@users.noreply.github.com>
2025-05-12 19:08:19 +0200

    removed unused import in blob

--- src/components/project-helpers/Blobs.tsx
+++ src/components/project-helpers/Blobs.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { navLinks, techIcons } from "../../constants";
+import { techIcons } from "../../constants";
 
 interface TechBlobProps {
   technologies: string[];
